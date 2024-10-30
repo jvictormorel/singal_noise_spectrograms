ff67523e67bd677b68731eacff64748255f0686e
jvictormorel <jvmorelrodrigues@gmail.com>
2024-10-30 09:14:21 -0300

    all of the spectograms are done

--- speech_factory/speech_factory.py
+++ speech_factory/speech_factory.py
@@ -4,86 +4,92 @@ from IPython.display import Audio
 import librosa
 import librosa.display
 
-# high quality graphics
-
-plt.rcParams['figure.dpi'] = 300
-
 # find the sampling rate of signals
 
-noise_babble, fs_babble = librosa.load('babble.wav', sr=None)
+clean_speech, fs_clean_speech = librosa.load(r'C:\Users\jvmor\OneDrive\Área de Trabalho\códigos_PIBIC_2024_2025\singal_noise_spectrograms\speech_factory\clean_speech.wav', sr=None)
 
-noise_cafeteria, fs_cafeteria = librosa.load('cafeteria.wav', sr=None)
+factory_0dB, fs_0dB = librosa.load(r'C:\Users\jvmor\OneDrive\Área de Trabalho\códigos_PIBIC_2024_2025\singal_noise_spectrograms\speech_factory\speech_factory_0dB.wav', sr=None)
 
-noise_factory, fs_factory = librosa.load('factory.wav', sr=None)
+factory_5dB, fs_5dB = librosa.load(r'C:\Users\jvmor\OneDrive\Área de Trabalho\códigos_PIBIC_2024_2025\singal_noise_spectrograms\speech_factory\speech_factory_5dB.wav', sr=None)
 
-audio_clean, fs_clean = librosa.load('clean_speech.wav', sr=None)
-
-audio_signal_5, fs_5 = librosa.load('speech_babble_0db.wav', sr=None)
+factory_m5dB, fs_m5dB = librosa.load(r'C:\Users\jvmor\OneDrive\Área de Trabalho\códigos_PIBIC_2024_2025\singal_noise_spectrograms\speech_factory\speech_factory_m5dB.wav', sr=None)
 
 # noise's spectograms
 
-D_babb = librosa.amplitude_to_db(np.abs(librosa.stft(noise_babble)))
+D_clean = librosa.amplitude_to_db(np.abs(librosa.stft(clean_speech)))
 plt.figure(figsize=(10, 4))
-librosa.display.specshow(D_babb, x_axis='time', y_axis='linear', sr=fs_babble, cmap='jet')
+librosa.display.specshow(D_clean, x_axis='time', y_axis='linear', sr=fs_clean_speech, cmap='jet')
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Babble Spectrogram')
+plt.title('Clean Speech Spectrogram')
 plt.show()
 
-D_caft = librosa.amplitude_to_db(np.abs(librosa.stft(noise_cafeteria)))
+D_0dB = librosa.amplitude_to_db(np.abs(librosa.stft(factory_0dB)))
 plt.figure(figsize=(10, 4))
-librosa.display.specshow(D_caft, x_axis='time', y_axis='linear', sr=fs_cafeteria, cmap='jet')
+librosa.display.specshow(D_0dB, x_axis='time', y_axis='linear', sr=fs_0dB, cmap='jet')
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Cafeteria Spectrogram')
+plt.title('Speech Factory 0dB Spectrogram')
 plt.show()
 
-D_fact = librosa.amplitude_to_db(np.abs(librosa.stft(noise_factory)))
+D_5dB = librosa.amplitude_to_db(np.abs(librosa.stft(factory_5dB)))
 plt.figure(figsize=(10, 4))
-librosa.display.specshow(D_fact, x_axis='time', y_axis='linear', sr=fs_factory, cmap='jet')
+librosa.display.specshow(D_5dB, x_axis='time', y_axis='linear', sr=fs_5dB, cmap='jet')
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Factory Spectrogram')
+plt.title('Speech Factory 5dB Spectrogram')
 plt.show()
 
-# speech's spectograms
-
-D_4 = librosa.amplitude_to_db(np.abs(librosa.stft(audio_signal_4)))
+D_m5dB = librosa.amplitude_to_db(np.abs(librosa.stft(factory_m5dB)))
 plt.figure(figsize=(10, 4))
-librosa.display.specshow(D_4, x_axis='time', y_axis='linear', sr=fs_4, cmap='jet')
+librosa.display.specshow(D_m5dB, x_axis='time', y_axis='linear', sr=fs_m5dB, cmap='jet')
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
 plt.colorbar(format='%+2.0f dB')
-plt.title('Spectrogram speech')
-plt.show()
-
-D_5 = librosa.amplitude_to_db(np.abs(librosa.stft(audio_signal_5)))
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(D_5, x_axis='time', y_axis='linear', sr=fs_5, cmap='jet')
-plt.xlabel('Time [s]')
-plt.ylabel('Frequency [Hz]')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Spectrogram speech_babble')
+plt.title('Speech Factory m5dB Spectrogram')
 plt.show()
 
 # amplitude x time
 
-time = np.linspace(0, len(audio_signal_1) / fs_1, num=len(audio_signal_1))
+time = np.linspace(0, len(clean_speech) / fs_clean_speech, num=len(clean_speech))
 plt.figure(figsize=(12, 4))
-plt.plot(time, audio_signal_1, color='blue')
-plt.title('Sinal de Áudio no Tempo')
-plt.xlabel('Tempo (s)')
+plt.plot(time, clean_speech, color='blue')
+plt.title('Clean Speech')
+plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.xlim(0, time[-1])  
 plt.grid()
 plt.show()
 
+time = np.linspace(0, len(factory_0dB) / fs_0dB, num=len(factory_0dB))
+plt.figure(figsize=(12, 4))
+plt.plot(time, factory_0dB, color='blue')
+plt.title('Speech Factory 0dB')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.xlim(0, time[-1])  
+plt.grid()
+plt.show()
 
+time = np.linspace(0, len(factory_5dB) / fs_5dB, num=len(factory_5dB))
+plt.figure(figsize=(12, 4))
+plt.plot(time, factory_5dB, color='blue')
+plt.title('Speech Factory 5dB')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.xlim(0, time[-1])  
+plt.grid()
+plt.show()
 
-
-
-
-
+time = np.linspace(0, len(factory_m5dB) / fs_m5dB, num=len(factory_m5dB))
+plt.figure(figsize=(12, 4))
+plt.plot(time, factory_m5dB, color='blue')
+plt.title('Speech Factory m5dB')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.xlim(0, time[-1])  
+plt.grid()
+plt.show()
